f7228cd912107aeb494790bd82292728878a831f
zbigniew.gawronski <zbigniew.gawronski@PK3-20>
2021-10-03 11:03:37 +0200

    Dodaj pliki projektów.

---
+++ Lab1a/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab1a.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
